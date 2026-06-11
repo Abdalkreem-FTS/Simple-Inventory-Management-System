bb4baa1e5017c302fa9e4da841e5dab69a4c9e8f
Abdalkreem Bzoor <abdalkreem.bzoor@foothillsolutions.com>
2026-06-11 11:14:36 +0300

    Add Product class

---
+++ IMS/Core/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace IMS.Core;
+
+/// <summary>
+/// Represents a single product in the inventory.
+/// </summary>
+public sealed class Product(string name, decimal price, int quantity)
+{
+    public string Name { get; set; } = name;
+    public decimal Price { get; set; } = price;
+    public int Quantity { get; set; } = quantity;
+}
