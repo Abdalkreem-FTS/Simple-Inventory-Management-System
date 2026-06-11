aed9cfddbc569bd0e9cd0be085847cf6ddd0cec2
Abdalkreem Bzoor <abdalkreem.bzoor@foothillsolutions.com>
2026-06-11 11:33:00 +0300

    Add 'view all products' feature

---
+++ IMS/Commands/ViewProductsCommand.cs
@@ -0,0 +1,20 @@
+﻿using IMS.Core;
+using IMS.UI;
+using Spectre.Console;
+
+namespace IMS.Commands;
+
+/// <summary>Renders the full product list as a formatted table.</summary>
+public sealed class ViewProductsCommand : ICommand
+{
+    public string Label => "[steelblue1]=[/]  View all products";
+
+    public bool Execute(Inventory inventory)
+    {
+        ConsoleUi.PrintSectionHeading("All Products");
+        ConsoleUi.RenderTable(inventory.GetProducts());
+        AnsiConsole.WriteLine();
+
+        return true;
+    }
+}
